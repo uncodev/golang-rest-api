f685803ec68987c65f1c9f9ddae4d1a683f71e3e
saeed <saeed.0347@gmail.com>
2018-11-22 20:42:00 +0330

    added mux

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
-import "fmt"
+import (
+    "net/http"
+    "log"
+    "github.com/gorilla/mux"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Gorilla!\n"))
+}
 
 func main() {
-	fmt.Println("Hello, uncodev")
+    r := mux.NewRouter()
+    // Routes consist of a path and a handler function.
+    r.HandleFunc("/", YourHandler)
+
+    // Bind to a port and pass our router in
+    log.Fatal(http.ListenAndServe(":8000", r))
 }
